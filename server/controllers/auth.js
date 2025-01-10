83031f45f576dac667e307c939c75f82f6b72782
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2025-01-10 18:24:47 +0000

    settin up server, authorization

---
+++ server/controllers/auth.js
@@ -0,0 +1,54 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName, lastName,
+            email, password, picturePath,
+            friends, location, occupation,
+        } = req.body
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName, lastName,
+            email, password: passwordHash, picturePath,
+            friends, location, occupation,
+            viewProfile: Math.floor(Math.random() * 1000),
+            impressions: Math.floor(Math.random() * 1000)
+        })
+
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email })
+
+        if (!user) return res.status(404).json({ msg: "User does not exist" })
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ msg: "Invalide credentials. " })
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+         // COMMENT: Destructure the password and reconstruct the user object without it
+        //    const { password: _, ...userWithoutPassword } = user.toObject(); 
+        //     res.status(200).json({ token, user: userWithoutPassword }); 
+
+        const { password: pass, ...rest } = user._doc;
+        res.status(200).json({ token, user: rest })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,74 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path'; // comes from node
+import { fileURLToPath } from 'url'
+import { register } from './controllers/auth.js';
+import  authRoutes  from './routers/auth.js'
+
+// CONFIGURATION =======
+// =========================
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+// these are used because the type int our package.json is set to module
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+// FILE STOAGE =========== 
+// =======================
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage })
+
+// ROUTES WITH FILES  =========
+//=============================
+app.post('/auth/register', upload.single("picture"), register) 
+
+// ROUTES   ===================
+//=============================
+app.use('/auth', authRoutes)
+
+// MONGOOSE SETUP =========
+//=========================
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(' Mongodb is connected')
+        app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
+    })
+    .catch((error) => console.log({ ' Mongodb connection error': error }))
+
+// SERVER START MESSAGE  =========
+//================================
+  app.get('/', (req, res) => {
+    res.send('Server is running!')
+  })
+ 
