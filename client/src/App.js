f4b69cefa8e0dd71779a14ba133c95c8e64c6997
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2025-01-11 21:07:34 +0000

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from "theme";
+import Navbar from "scenes/navbar";
 
 function App() {
   const mode = useSelector((state) => state.mode)
@@ -17,6 +18,7 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Navbar />
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/home" element={<HomePage />} />
