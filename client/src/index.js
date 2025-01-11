278dd85391d0acf417b92a2c51b127e9b844c897
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2025-01-11 17:21:09 +0000

    create pages of frontend, create redux configs

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import authReducer from './state'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+/* STORE.JS FILE configuration is inside index.js  */
+/* ==============================================  */
+/* there is no rootReducer which is created using combineReducers,
+ because there is only one reducer called authReducer */
+persistConfig = { key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer) /* if there was a rootReducer, it would be here instead of authReducer */
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
