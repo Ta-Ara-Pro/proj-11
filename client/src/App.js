84151a10733a3646756546f4a0f92086e82363a2
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2025-01-12 20:41:25 +0000

    edit private route conditional variable

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Navbar from "scenes/navbar";
 function App() {
   const mode = useSelector((state) => state.mode)
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.user))
+  const isAuth = Boolean(useSelector((state) => state.token))
 
   return (
     <div className="app">
